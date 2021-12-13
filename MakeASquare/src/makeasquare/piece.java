f55a4e9bf9469abd9aee8e29f43a7672017332a0
Marwan <Marwan.Nabil.ElDeep@gmail.com>
2021-12-14 01:26:15 +0200

    added class piece that contains move0 , move1 and some small features.

--- MakeASquare/src/makeasquare/piece.java
+++ MakeASquare/src/makeasquare/piece.java
@@ -1,7 +1,65 @@
 
 package makeasquare;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+discribe anything you need here.
+*/
 
 public class piece {
+    private int row , col;
+    private char[][] piece;
+    public piece(int row , int col , char[][] piece){
+        this.row = row;
+        this.col = col;
+        this.piece = piece;
+    }
+    public char[][] getPiece(){
+        return this.piece.clone();
+    }
     
+    public char[][] getPieceAfterRotation(int moveID) {
+        
+        switch(moveID){
+            case 0:
+                return this.move0();
+            case 1:
+                return this.move1();
+
+        }
+        System.out.print("!!");
+        throw new IndexOutOfBoundsException();
+    }
+    
+    private char[][] move0(){
+        return this.piece;
+    }
+    private char[][] move1(){
+        /*
+        it's like rotating it 90 degree clock-wise.
+        */
+        int id = this.row - 1;
+        char[][] newPiece = new char[col][row];
+        for(int i = 0; i < this.row; i++){
+            for(int j = 0; j < this.col; j++){
+                newPiece[j][id] = this.piece[i][j];
+            }
+            id--;
+        }
+        return newPiece;
+    }
+    
+    @Override
+    public String toString(){
+        StringBuffer ret = new StringBuffer();
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                ret.append(this.piece[i][j]);
+            }
+            ret.append('\n');
+        }
+        return ret.toString();
+    }
 }
