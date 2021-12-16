105b8d2a6c73ef2d9e68213ff752f00b44755221
Saytara2001 <lionheartsaytara@gmail.com>
2021-12-16 12:42:23 +0200

    last commit

--- MakeASquare/src/makeasquare/MakeASquare.java
+++ MakeASquare/src/makeasquare/MakeASquare.java
@@ -3,9 +3,28 @@ package makeasquare;
 
 public class MakeASquare {
 
-    
+
     public static void main(String[] args) {
-        
+        char[][] piece = {{'0' , '1'} , {'2' , '3'}};
+        piece trail = new piece(2 , 2 , piece);
+
+        char[][] ret = trail.getPieceAfterRotation(1);
+
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 2; j++){
+                System.out.print(ret[i][j]);
+            }
+            System.out.println();
+        }
+
     }
-    
+
 }
+/*
+01
+23
+
+20
+31
+
+*/
